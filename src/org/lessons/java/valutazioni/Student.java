c366d3e7f57f884edf6fd5d3e9b431e2249017ef
CampanelliEmanuele <emanuele.campanelli.work@gmail.com>
2023-12-13 16:32:28 +0100

    Add: toString()

--- src/org/lessons/java/valutazioni/Student.java
+++ src/org/lessons/java/valutazioni/Student.java
@@ -5,15 +5,13 @@ public class Student {
     private int absencesPercentage;
     private double gradeAverage;
 
-    /* CONTRACTORS */
-
     public Student(int id, int absencesPercentage, double gradeAverage) throws IllegalArgumentException {
         setId(id);
         setAbsencesPercentage(absencesPercentage);
         setGradeAverage(gradeAverage);
     }
 
-    /* MAIN FUNCTIONS */
+    /* EXERCISE FUNCTIONS */
 
     public boolean isPassed() {
         if (absencesPercentage > 50)            /* 100 >= absencesPercentage > 50 */
@@ -24,19 +22,26 @@ public class Student {
             return gradeAverage >= 2;
     }
 
+    @Override
+    public String toString() {
+        return "Student\t\tid: " + id +
+                "\tabsencesPercentage: " + absencesPercentage +
+                "\tgradeAverage: " + gradeAverage;
+    }
+
     /* VALIDATORS */
 
-    private void validId(int id) {
+    private void validId(int id) throws IllegalArgumentException {
         if (id <= 0)
             throw new IllegalArgumentException("Error: student id cannot be a negative number or equal to 0.");
     }
 
-    private void validAbsencesPercentage(int absencesPercentage) {
+    private void validAbsencesPercentage(int absencesPercentage) throws IllegalArgumentException {
         if (absencesPercentage < 0 || absencesPercentage > 100)
             throw new IllegalArgumentException("Error: student absences percentage must be between 0 and 100 (inclusive).");
     }
 
-    private void validGradeAverage(double gradeAverage) {
+    private void validGradeAverage(double gradeAverage) throws IllegalArgumentException {
         if (gradeAverage < 0 || gradeAverage > 5)
             throw new IllegalArgumentException("Error: student grade average must be between 0 and 5 (inclusive).");
     }
@@ -47,7 +52,7 @@ public class Student {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(int id) throws IllegalArgumentException {
         validId(id);
         this.id = id;
     }
@@ -56,7 +61,7 @@ public class Student {
         return absencesPercentage;
     }
 
-    public void setAbsencesPercentage(int absencesPercentage) {
+    public void setAbsencesPercentage(int absencesPercentage) throws IllegalArgumentException {
         validAbsencesPercentage(absencesPercentage);
         this.absencesPercentage = absencesPercentage;
     }
@@ -65,7 +70,7 @@ public class Student {
         return gradeAverage;
     }
 
-    public void setGradeAverage(double gradeAverage) {
+    public void setGradeAverage(double gradeAverage) throws IllegalArgumentException {
         validGradeAverage(gradeAverage);
         this.gradeAverage = gradeAverage;
     }
